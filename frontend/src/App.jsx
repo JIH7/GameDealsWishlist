b309b83cedc122ec9941141008e35ff010b3e4ec
Jeremy <jihlucky7@gmail.com>
2025-02-17 15:00:05 -0600

    Refactored App back into a function component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,29 @@
-import { useState, Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
 import './App.css';
 
 import HomePage from './pages/home/home_page';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [gameList, setGameList] = useState();
 
-    const[gamesList, setGamesList] = useState();
-  }
-
-  async componentDidMount() {
-    await this.refreshList()
-  }
-
-  async refreshList() {
+  const UpdateList = async () => {
     try {
       let response = await axios.get("http://localhost:8000/api/games/");
-      setGamesList(response.data);
-      console.log(response.data);
     }
-    catch(error) {
-      console.log(error);
+    catch (error) {
+      console.log(error)
     }
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <HomePage />
-      </>
-    );
-  }
+  useEffect(() => UpdateList(), []);
+
+  return (
+    <>
+      <HomePage />
+    </>
+  );
 }
+
 
 export default App;
