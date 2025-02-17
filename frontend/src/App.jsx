ab048a5019b31cc81b23db44a82ae998db6949b0
Jeremy <jihlucky7@gmail.com>
2025-02-17 14:41:10 -0600

    Fixed coors whitelist issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react';
+import { useState, Component } from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <div></div>
-    </>
-  )
-};
+import HomePage from './pages/home/home_page';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const[gamesList, setGamesList] = useState();
+  }
+
+  async componentDidMount() {
+    await this.refreshList()
+  }
+
+  async refreshList() {
+    try {
+      let response = await axios.get("http://localhost:8000/api/games/");
+      setGamesList(response.data);
+      console.log(response.data);
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <HomePage />
+      </>
+    );
+  }
+}
 
 export default App;
---
+++ frontend/src/pages/home/home_page.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <>
+            <h1>Games List</h1>
+
+        </>
+    )
+}
+
+export default HomePage;
